b2d4247c6a18133786b1cd740382bcd950675d42
edward9s <277983297+edward9s@users.noreply.github.com>
2026-09-21 15:12:08 +0800

    Raise Cleric cooperation bonus

--- core/src/main/java/com/spd/cohero/CoHeroClassTraits.java
+++ core/src/main/java/com/spd/cohero/CoHeroClassTraits.java
@@ -12,7 +12,7 @@ public final class CoHeroClassTraits {
     private static final float HUNTRESS_MISSILE_DURABILITY_MULTIPLIER = 1.2f;
     private static final float DUELIST_MELEE_SPEED_MULTIPLIER = 1.09051f;
     private static final float CLERIC_SOLO_MULTIPLIER = 1.10f;
-    private static final float CLERIC_COOP_MULTIPLIER = 1.25f;
+    private static final float CLERIC_COOP_MULTIPLIER = 1.30f;
     public static final int CLERIC_AURA_RANGE = 3;
     private static final float GENERALIST_HT_MULTIPLIER = 1.05f;
 
