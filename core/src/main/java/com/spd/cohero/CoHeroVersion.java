89dd68bb9750959ff92b66c37ae3bd758a904d7f
edward9s <277983297+edward9s@users.noreply.github.com>
2026-09-22 23:10:27 +0800

    Update version to 0.1.1

--- core/src/main/java/com/spd/cohero/CoHeroVersion.java
+++ core/src/main/java/com/spd/cohero/CoHeroVersion.java
@@ -8,7 +8,7 @@ package com.spd.cohero;
  */
 public final class CoHeroVersion {
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 
     private CoHeroVersion() {
     }
