fe4e792a4c2df22e42bcdc23ed20388ac854aedd
edward9s <277983297+edward9s@users.noreply.github.com>
2026-09-24 17:13:57 +0800

    Bump version from 0.1.2 to 0.1.3

--- core/src/main/java/com/spd/cohero/CoHeroVersion.java
+++ core/src/main/java/com/spd/cohero/CoHeroVersion.java
@@ -8,7 +8,7 @@ package com.spd.cohero;
  */
 public final class CoHeroVersion {
 
-    public static final String VERSION = "0.1.2";
+    public static final String VERSION = "0.1.3";
 
     private CoHeroVersion() {
     }
